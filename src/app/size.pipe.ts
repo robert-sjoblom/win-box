63bd547384f60aba528cc0ef9ce0406192ba98a0
Robert Sjoblom <robert.sjoblom@gmail.com>
2018-05-23 10:54:13 +0200

    removed useless test files becoz we good like dat. /s

--- src/app/size.pipe.ts
+++ src/app/size.pipe.ts
@@ -6,19 +6,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class SizePipe implements PipeTransform {
 
   transform(value: any, args?: any): any {
-    if (value === NaN || value === undefined || value === ""){
-      return "";
+    if (value === NaN || value === undefined || value === '') {
+      return '';
     }
-    let sizeNr = Number(value)/1024;
-    if(sizeNr > 1){
-      if (sizeNr/1024 > 1){
-        sizeNr = sizeNr/1024
-        return `${sizeNr.toFixed(2)} MB`
+    let sizeNr = Number(value) / 1024;
+    if (sizeNr > 1) {
+      if (sizeNr / 1024 > 1) {
+        sizeNr = sizeNr / 1024;
+        return `${sizeNr.toFixed(2)} MB`;
       }
-      return `${sizeNr.toFixed(2)} KB`
-    } else{
-      sizeNr = sizeNr*1024;
-      return `${sizeNr} byte`
+      return `${sizeNr.toFixed(2)} KB`;
+    } else {
+      sizeNr = sizeNr * 1024;
+      return `${sizeNr} byte`;
     }
   }
 }
